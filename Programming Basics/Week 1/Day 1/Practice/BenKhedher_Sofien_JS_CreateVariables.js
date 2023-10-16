a88d144c7536a03b699dcf4bccd14a146e14ea5c
Sofien-BK <dmarket0707@gmail.com>
2023-10-16 15:36:09 +0200

    commit

---
+++ Programming Basics/Week 1/Day 1/Practice/BenKhedher_Sofien_JS_CreateVariables.js
@@ -0,0 +1,12 @@
+var age =0;
+var heigth =0;
+function rolleCoaster (age , heigth){
+    if ((age>11) && (heigth>=42)) {
+    console.log("Have fun");
+    }
+    else {
+ console.log("Maybe next time");
+    }
+}   
+
+
