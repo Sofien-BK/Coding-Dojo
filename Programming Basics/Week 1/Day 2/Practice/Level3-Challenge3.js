f8fc32f7561d164c3aa9dbc97167e071764cdd5c
Sofien-BK <dmarket0707@gmail.com>
2023-10-17 15:40:41 +0200

    commit

---
+++ Programming Basics/Week 1/Day 2/Practice/Level3-Challenge3.js
@@ -0,0 +1,3 @@
+var x = [1,3,5,8];
+x[0] = x[x.length-1]; 
+console.log(x); // -- > [8,3,5,8] 
---
+++ Programming Basics/Week 1/Day 2/Practice/Level3-Challenge5.js
@@ -0,0 +1,5 @@
+var x = [1,3,5,8];
+var temp = x [x.length-1];
+x[x.length-1]=x[0];
+x[0]=temp;
+console.log(x); // --> [8,3,5,1] 
