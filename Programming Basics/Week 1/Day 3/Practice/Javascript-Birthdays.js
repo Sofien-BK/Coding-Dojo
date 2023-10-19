e7024ad598c01d3b8430995b64c2134a7c434be9
Sofien-BK <dmarket0707@gmail.com>
2023-10-19 14:10:10 +0200

    commit

---
+++ Programming Basics/Week 1/Day 3/Practice/Javascript-Birthdays.js
@@ -0,0 +1,47 @@
+/* ------------------ group activity -----------
+
+    write a for-loop that starts at year 1996 and ends in 2023
+    on every year (iteration) have it log the year and the string "JS is awesome!"
+    on every 10 years, log "happy decade of JS!"
+    when you reach the end, log "HAPPY 27 years of JAVASCRIPT! 🎈🎈🎈"
+
+    ex. "1996 JS is awesome"
+        "1997 JS is awesome"
+        ...
+        "2004 JS is awesome"
+        "2005 JS is awesome"
+        "happy decade of JS!"
+        "2007 JS is awesome"
+        ....
+        "2022 JS is awesome"
+        "HAPPY 27 years of JAVASCRIPT! 🎈🎈🎈"
+
+ */
+for (var year = 1996; year<=2023; year++){
+    for (var count1=0; count1<=100; count1++){
+        count1+=1;
+
+    var x = year + " JS is awesome"
+    var y = "happy decade of JS!"
+    var count = [10,20,30,40,50,60,70,80,90,100];
+    
+    
+    console.log(count1);
+    console.log(x);
+     if (count1 == count){
+         console.log(y);
+     }
+    } 
+}
+
+
+
+
+    
+
+
+
+
+
+
+
---
+++ Programming Basics/Week 1/Day 4/Practice/BenKhedher_Sofien_LoopDLoop.js
@@ -0,0 +1,12 @@
+// we need a loop because we need to do things from a specific strat to a specific end.
+// the starting point of the loop is reaching 2 miles
+// when the runner reach 6 miles the loop should stop
+// We specify the stop point in the loop
+// the variable number that suposed to be the counter in the loop 
+// variables of numbers 
+for (var i = 0 ; i<=6 ; i++){
+    if ((i>0) && (i%2 == 0)){
+        console.log("Take Your Candy :)")
+    }
+    
+}
