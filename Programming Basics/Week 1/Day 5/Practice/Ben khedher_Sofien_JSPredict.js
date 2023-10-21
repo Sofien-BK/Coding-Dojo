45b0d1264743b7dbbdebc1a12b2f1f6ba13e3ab7
Sofien-BK <dmarket0707@gmail.com>
2023-10-21 07:26:52 +0200

    JSPredict

---
+++ Programming Basics/Week 1/Day 5/Practice/Ben khedher_Sofien_JSPredict.js
@@ -0,0 +1,6 @@
+// the console.log will state --> I was born in 1980 
+// If this function is called with the argument 1980 , it will state --> I was born in 1980 
+// console.log will state --> summing numbers!
+                            //num1 is 10
+                            //num2 is 20
+                            // 30
