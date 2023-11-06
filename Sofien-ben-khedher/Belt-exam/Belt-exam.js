714456e4a914de25e2311db687e8378ba6b80f9b
Sofien-BK <dmarket0707@gmail.com>
2023-11-06 12:29:04 +0000

    Belt-exam

---
+++ Sofien-ben-khedher/Belt-exam/Belt-exam.js
@@ -0,0 +1,9 @@
+var element = document.querySelector("#join")
+function remove (){
+    element.remove()
+}
+var message = document.querySelector(".s-s") 
+
+function alertt(){
+    alert("You are searching for " + message.value)
+}
